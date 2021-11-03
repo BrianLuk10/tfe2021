c4e9cf009d83187de6800db76b7b0591cb560ea4
BrianLuk10 <brianluk1412@gmail.com>
2021-11-03 02:21:24 +0100

    eletron react debut

---
+++ src/js/App.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+export default function App(){
+
+    return(
+        <>
+        <h1>Je suis un component 2</h1>
+        <button onClick={() => {alert('gg')}}>slt</button>
+        </>
+    )
+};
