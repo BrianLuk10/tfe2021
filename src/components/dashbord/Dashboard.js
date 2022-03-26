65cb30f77306775c9043e06f2957d46e0d54b6ae
BrianLuk10 <56024639+BrianLuk10@users.noreply.github.com>
2022-03-26 02:34:13 +0100

    correction bug chart affichage + date

--- src/components/dashbord/Dashboard.js
+++ src/components/dashbord/Dashboard.js
@@ -61,6 +61,7 @@ class Create extends Component {
     this.state = {
       total: [],
       date: [],
+      isReady: false,
     };
   }
 
@@ -68,15 +69,11 @@ class Create extends Component {
     axios.get("http://localhost:3030/commande/days").then((res) => {
       for (let i = 0; i < res.data.length; i++) {
         this.state.total.push(res.data[i].total);
-      }
-      for (let i = 5; i > -2; i--) {
-        var curr = new Date(); // get current date
-        var first = curr.getDate() - curr.getDay(); // First day is the day of the month - the day of the week
-        var last = first + i;
-        let date = new Date(curr.setDate(last)).toGMTString().slice(0, 16);
-        this.state.date.push(date);
+        this.state.date.push(res.data[i].date_commandes.slice(0, 10));
       }
     });
+
+    this.setState({ isReady: true });
   }
 
   /*
@@ -107,37 +104,41 @@ class Create extends Component {
   render() {
     return (
       <div>
-        <Bar
-          data={{
-            labels: this.state.date,
-            datasets: [
-              {
-                label: "Tableau total de la semaine en €",
-                data: this.state.total,
-                backgroundColor: [
-                  "rgba(255, 99, 132, 0.2)",
-                  "rgba(255, 159, 64, 0.2)",
-                  "rgba(255, 205, 86, 0.2)",
-                  "rgba(75, 192, 192, 0.2)",
-                  "rgba(54, 162, 235, 0.2)",
-                  "rgba(153, 102, 255, 0.2)",
-                  "rgba(201, 203, 207, 0.2)",
-                ],
-                borderColor: [
-                  "rgb(255, 99, 132)",
-                  "rgb(255, 159, 64)",
-                  "rgb(255, 205, 86)",
-                  "rgb(75, 192, 192)",
-                  "rgb(54, 162, 235)",
-                  "rgb(153, 102, 255)",
-                  "rgb(201, 203, 207)",
-                ],
-                borderWidth: 1,
-              },
-            ],
-          }}
-          redraw={true}
-        />
+        {this.state.isFetching ? (
+          <div>Loading...</div>
+        ) : (
+          <Bar
+            data={{
+              labels: this.state.date,
+              datasets: [
+                {
+                  label: "Tableau total de la semaine en €",
+                  data: this.state.total,
+                  backgroundColor: [
+                    "rgba(255, 99, 132, 0.2)",
+                    "rgba(255, 159, 64, 0.2)",
+                    "rgba(255, 205, 86, 0.2)",
+                    "rgba(75, 192, 192, 0.2)",
+                    "rgba(54, 162, 235, 0.2)",
+                    "rgba(153, 102, 255, 0.2)",
+                    "rgba(201, 203, 207, 0.2)",
+                  ],
+                  borderColor: [
+                    "rgb(255, 99, 132)",
+                    "rgb(255, 159, 64)",
+                    "rgb(255, 205, 86)",
+                    "rgb(75, 192, 192)",
+                    "rgb(54, 162, 235)",
+                    "rgb(153, 102, 255)",
+                    "rgb(201, 203, 207)",
+                  ],
+                  borderWidth: 1,
+                },
+              ],
+            }}
+            redraw={true}
+          />
+        )}
       </div>
     );
   }
